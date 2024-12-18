1e421400a291666a2455d6bf7e4989a502b943c4
wiam <wiambelarabi10@gmail.com>
2024-12-18 16:57:14 +0100

    initial commit

--- DHT/api.py
+++ DHT/api.py
@@ -5,13 +5,17 @@ from rest_framework import status
 from rest_framework.response import Response
 from django.core.mail import send_mail
 from django.conf import settings
+person_counter = 0
 
 @api_view(["GET", "POST"])
 def Dlist(request):
+    global person_counter
+
     if request.method == "GET":
-        all_data = Dht11.objects.all()
-        data_ser = DHT11serialize(all_data, many=True)  # Les données sont sérialisées en JSON
+        all = Dht11.objects.all()
+        data_ser = DHT11serialize(all, many=True)  # Les données sont sérialisées en JSON
         return Response(data_ser.data)
+
     elif request.method == "POST":
         serial = DHT11serialize(data=request.data)
 
@@ -20,7 +24,6 @@ def Dlist(request):
             derniere_temperature = Dht11.objects.last().temp
             print(derniere_temperature)
 
-            if serial.is_valid():
-                serial.save()
-                derniere_temperature = Dht11.objects.last().temp
-                print(derniere_temperature)
+            return Response(serial.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
